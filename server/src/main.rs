d558b71875c7e9023cfea128aed9380e72bc83b2
Alvaro Tolentino <alvarotolentinou@gmail.com>
2026-03-30 14:50:31 -0500

    feat: add tikv-jemallocator as global allocator for improved memory management

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,6 @@
+#[global_allocator]
+static GLOBAL: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
+
 use clap::{Parser, ValueEnum};
 use std::sync::atomic::Ordering;
 use std::sync::Arc;
